2c93f1383f43b11cdc236d3bde328a1bcc5fbd14
Haruna5718 <devharuna5718@gmail.com>
2026-04-01 03:06:07 +0900

    Space Size add

--- frontend/src/lib/Space.svelte
+++ frontend/src/lib/Space.svelte
@@ -2,6 +2,8 @@
     export let data: { [key: string]: any };
 </script>
 <span style="
-    flex-grow: {data.attr.factor ?? 1};">
+    flex-grow: {data.attr.size == null ? (data.attr.factor ?? 1) : 0};
+    flex-shrink: {data.attr.size == null ? 1 : 0};
+    flex-basis: {data.attr.size ?? 'auto'};">
     <slot />
 </span>
